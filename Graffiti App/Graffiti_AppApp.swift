a2162f5b89b1bfef07c8315bcf857e584e22d378
Giselleburp <tan_xin_xuan_giselle@students.edu.sg>
2025-08-30 15:37:02 +0800

    Initial Commit

---
+++ Graffiti App/Graffiti_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graffiti_AppApp.swift
+//  Graffiti App
+//
+//  Created by Sam Tan on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Graffiti_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
